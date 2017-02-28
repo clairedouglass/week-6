3b2ff8c4642df15c3e0c0ed4cec8360fc1dbe83a
Claire Douglass <cldoug@upenn.edu>
2017-02-27 22:12:18 -0500

    MidtermRound2

--- assignment/js/midtermFunctions.js
+++ assignment/js/midtermFunctions.js
@@ -6,6 +6,14 @@ Week 6 Assignment: Midterm Functions + Signatures
 Leaflet Configuration
 ===================== */
 
+//Questions:
+//How to attach the slide functions into the slide numbers?
+//I have a function(slideView) which says when each slide number appears, call a specific function - but the function doesn't run on it's own
+//I need the function to reference the parsed data - I can put in the ajax call, but then the layers overlap
+//Should I add layers and then remove them? Is there a cleaner way to do it?
+//Can I call the functions in a way that "resets" the map each time?
+
+
 var map = L.map('map', {
   center: [40.733110, -73.980036],
   zoom: 11
@@ -23,15 +31,17 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
 var dataset = "https://raw.githubusercontent.com/clairedouglass/midterm/master/Subway%20Lines.geojson";
 var featureGroup;
 
+var parsedData;
+
 // Parse GeoJSON file and add lines to map with styling
 $(document).ready(function() {
   $.ajax(dataset).done(function(data) {
-    var parsedData = JSON.parse(data);
-    featureGroup = L.geoJson(parsedData, {
-    style: slideView
-  }).addTo(map);
+    parsedData = JSON.parse(data);
+    console.log(parsedData);
+    featureGroup = L.geoJson(parsedData, {style: myStyle1}).addTo(map);
   });
 });
+
 
 //define the state
 var state = {
@@ -62,60 +72,81 @@ var state = {
 
 //adds functions to slides
 var slideView = function(state) {
-  if (state.slideNumber === "1") {
-    myStyle1();
+  if (state.slideNumber === 1) {
+    _.each(parsedData.features, function(feature){
+      featureGroup.setStyle({style: myStyle1(feature)});
+    });
   }
-  else if (state.slideNumber === "2") {
-    myStyle2();
+  else if (state.slideNumber === 2) {
+    _.each(parsedData.features, function(feature){
+      featureGroup.setStyle({style: myStyle2(feature)});
+    });
   }
-  else if (state.slideNumber === "3") {
+  else if (state.slideNumber === 3) {
     myStyle3();
   }
-  else if (state.slideNumber === "4") {
+  else if (state.slideNumber === 4) {
     myStyle4();
-    //zoom level
   }
-  else if (state.slideNumber === "5") {
+  else if (state.slideNumber === 5) {
     myStyle5();
-    //zoom level
   }
 };
 
+
+//Do I need these functions below??
+
+//Set the slide style
+var setStyle = function(state) {
+  $().css('style', slideView(state.slideNumber));
+};
+
+//Clear map
+var removeLines = function(data) {
+  _.each(data, function(marker) {
+    return map.removeLayer(marker);
+  });
+};
+//resetMap();
+
 //click next button function
-var clickNextButton = function(event) {
+var forward = function(event) {
+  console.log('Inside the click next button function');
   var limit = state.slideData.length - 1;
   if (state.slideNumber + 1 > limit) {
     state.slideNumber = 0;
   } else {
     state.slideNumber = state.slideNumber + 1;
   }
-  //return state.slideData[state.slideNumber];
   return state.slideNumber;
 };
 
 //pass function into button upon click
 $("#nextButton").click(function(event) {
-  clickNextButton();
+  console.log('Click Next Button');
+  forward();
+  slideView(state);
 });
 
 //click previous button
-var clickPreviousButton = function(event) {
+var backward = function(event) {
+  console.log('Inside');
   if (state.slideNumber - 1 < 0) {
     state.slideNumber = state.slideNumber.length - 1;
   } else {
     state.slideNumber = state.slideNumber - 1;
-    }
+  }
+  return state.slideNumber;
     //return state.slideData[state.slideNumber];
-    return state.slideNumber;
 };
 
 //pass function into button upon click
-$("#prevButton").click(function(event) {
-  clickPreviousButton();
+$("#prevButton").click(function(event){
+  console.log('Click Back Button');
+  backward();
+  slideView(state);
 });
 
-//map.setZoom
-//maybe earch of your slide have a zoom level
 
 /* ================================
 
@@ -126,8 +157,9 @@ $("#prevButton").click(function(event) {
 
 var myStyle1 = function(feature) {
     //showResults();
+    $(".main").replaceWith("<h2>New York City Subway Lines</h2>");
+    $("#content").replaceWith("<p>Subway lines are represented by their MTA colors</p>");
     var mainColor = feature.properties.rt_symbol;
-    console.log(feature.properties.rt_symbol);
     switch(mainColor) {
       case "1":
         return {color: 'red'};
@@ -155,6 +187,8 @@ var myStyle1 = function(feature) {
 //Logic: categorize rt_symbol values into three groups
 
 var myStyle2 = function(feature) {
+  $(".main").replaceWith("<h2>History: A Tale of Three Companies</h2>");
+  $("#content").replaceWith("<p>BMT, IRT</p>");
   var mainColor = feature.properties.rt_symbol;
   switch(mainColor) {
     case "1":
@@ -170,11 +204,14 @@ var myStyle2 = function(feature) {
     case "N":
       return {color: 'yellow'};
   }
+  setStyle()
 };
 
 // Function that colors the lines for slide 3
 //Logic: highlight routes using the "name" field
 var myStyle3 = function(feature) {
+  $(".main").replaceWith("<h2>Ongoing Superstorm Sandy Recovery Efforts</h2>");
+  $("#content").replaceWith("<p>Lines that are under construction</p>");
   var mainColor = feature.properties.rt_symbol;
   switch(mainColor) {
     case "L":
@@ -197,6 +234,8 @@ var myStyle4Zoom = function() {
 // Function that colors the lines for slide 4
 var myStyle4 = function(feature) {
   myStyle4Zoom();
+  $(".main").replaceWith("<h2>New Infrastructure: Hudson Yards Subway Stop</h2>");
+  $("#content").replaceWith("<p>Hudson Yards</p>");
   var mainColor = feature.properties.rt_symbol;
   switch(mainColor) {
     case "7":
@@ -215,6 +254,8 @@ var myStyle5Zoom = function() {
 // Function that colors the lines for slide 5
 var myStyle5 = function(feature) {
   myStyle5Zoom();
+  $(".main").replaceWith("<h2>New Subway Infrastructure: Second Avenue Subway</h2>");
+  $("#content").replaceWith("<p>Three new stops opened on January 1, 2017</p>");
   var mainColor = feature.properties.name;
   switch(mainColor) {
     case "Q":
